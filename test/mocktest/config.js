4ae246c464da2701ee54f2896ce0db21ecdd3310
PriyankaShivakumar <priyanka.r.s@sap.com>
2021-10-08 12:55:10 +0530

    Push to git mock

--- test/mocktest/config.js
+++ test/mocktest/config.js
@@ -8,6 +8,7 @@ mockserverurl = mockserverurl.join(".");
 
 module.exports = {
     "token_url": vcap.xsuaa[0].credentials.url,
+    "em_queue_url": vcap["enterprise-messaging"][0].credentials.messaging[2].uri + '/messagingrest/v1/topics/referenceappscf%2Femsf%2F1909%2Fsfemessage',
     "service_domain": 'https://' + appenv.application_uris[0],
     "mock_service_domain": 'https://' + mockserverurl,
     "xsuaa": {
@@ -17,5 +18,10 @@ module.exports = {
         "username": cred.pusername,
         "password": cred.puserpwd
 
-    }
+    },
+    "enterprise_messaging": {
+        "grant_type": "client_credentials",
+        "client_id": vcap["enterprise-messaging"][0].credentials.messaging[2].oa2.clientid,
+        "client_secret": vcap["enterprise-messaging"][0].credentials.messaging[2].oa2.clientsecret
+    },
 }
--- test/mocktest/test.js
+++ test/mocktest/test.js
@@ -3,11 +3,11 @@ const chaiHttp = require('chai-http');
 //const server = require('./server');
 const config = require('./config');
 let xsuaa_access_token;
-let arr;
-let businessspartner;
-let riskid;
-let bp;
-let autoRiskID;
+let em_access_token;
+let user;
+let userpic;
+let skill;
+
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
@@ -26,6 +26,162 @@ describe("get access token for xsuaa", () => {
                     try {
                         response.should.have.status(200);
                         xsuaa_access_token = response.body.access_token;
+                        done();
+                    } catch (err) {
+                        done(err);
+                    }
+                });
+        });
+    });
+});
+
+describe("get em access token", () => {
+    describe("Should Get access token for em", () => {
+        it(" should fetch access token", (done) => {
+
+            var req_headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+            chai.request(config.token_url)
+                .post('/oauth/token').set(req_headers).send(config.enterprise_messaging)
+                .end((error, response) => {
+                    try {
+                        response.should.have.status(200);
+                        em_access_token = response.body.access_token;
+                        done();
+                    } catch (err) {
+                        done(err);
+                    }
+                });
+        });
+    });
+});
+
+describe('Read Projects', () => {
+    describe('Should get all  Projects', () => {
+        it('+ should return a list of projects', (done) => {
+            chai.request(config.service_domain)
+                .get('/admin/Project').set('Authorization', 'bearer ' + xsuaa_access_token)
+                .end((error, response) => {
+                    try {
+                        console.log(response.body);
+                        response.should.have.status(200);
+                        done();
+                    } catch (err) {
+                        done(err);
+                    }
+                });
+        });
+    });
+});
+
+describe("Read Users", () => {
+
+    it('Read the User', (done) => {
+
+        let url = '/sfservice-mocks/Users';
+        chai.request(config.mock_service_domain)
+            .get(url).send('Content-Type', 'application/x-www-form-urlencoded')
+            .end((error, response) => {
+                try {
+                    response.should.have.status(200);
+                    user = response.body.value[0];
+                    console.log(user);
+                    setTimeout(() => {
+                        done();
+                    }, 1000);
+                } catch (err) {
+                    done(err);
+                }
+            });
+
+    })
+
+});
+
+describe("Read UserPhoto", () => {
+
+    it('Read the User Photo', (done) => {
+
+        let url = '/sfservice-mocks/Userphoto';
+        chai.request(config.mock_service_domain)
+            .get(url).send('Content-Type', 'application/x-www-form-urlencoded')
+            .end((error, response) => {
+                try {
+                    response.should.have.status(200);
+                    userpic = response.body.value[0];
+                    console.log(userpic);
+                    setTimeout(() => {
+                        done();
+                    }, 1000);
+                } catch (err) {
+                    done(err);
+                }
+            });
+
+    })
+
+});
+
+describe("Read Skills", () => {
+
+    it('Read the Skill', (done) => {
+
+        let url = '/sfservice-mocks/Skill';
+        chai.request(config.mock_service_domain)
+            .get(url).send('Content-Type', 'application/x-www-form-urlencoded')
+            .end((error, response) => {
+                try {
+                    response.should.have.status(200);
+                    skill = response.body.value[0];
+                    console.log(skill);
+                    setTimeout(() => {
+                        done();
+                    }, 1000);
+                } catch (err) {
+                    done(err);
+                }
+            });
+
+    })
+
+});
+
+
+describe("Post Notification", () => {
+    it(" Post Notification", (done) => {
+        let notification = {
+            "message": "Resigned",
+            "employeeId": "106020",
+            "managerId": config.xsuaa.username
+        }
+        var req_headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + em_access_token,
+            'x-qos': '1'
+        }
+        chai.request(config.em_queue_url)
+            .post('/messages').send(notification).set(req_headers)
+            .end((error, response) => {
+                try {
+                    //response.should.have.status(204);
+                    console.log(response.body);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            });
+    })
+});
+
+describe('Read Notifications', () => {
+    describe('Should get all  Notifications', () => {
+        it('+ should return a list of Notifications', (done) => {
+            let url = '/admin/Notifications';
+            chai.request(config.service_domain)
+                .get(url).set('Authorization', 'bearer ' + xsuaa_access_token)
+                .end((error, response) => {
+                    try {
+                        console.log(response.body);
+                        //response.should.have.status(200);
                         done();
                     } catch (err) {
                         done(err);
